e3bdd1810122f9658498641efdadaea34c60cc77
Kartulin <stalkerxxl@gmail.com>
2022-11-26 22:32:28 +0400

    make start fixtures load (country list)

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -2,16 +2,57 @@
 
 namespace App\DataFixtures;
 
+use App\Entity\Country;
+use App\Service\ApiClient;
+use DateTimeImmutable;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
+use Symfony\Contracts\HttpClient\Exception\ClientExceptionInterface;
+use Symfony\Contracts\HttpClient\Exception\DecodingExceptionInterface;
+use Symfony\Contracts\HttpClient\Exception\RedirectionExceptionInterface;
+use Symfony\Contracts\HttpClient\Exception\ServerExceptionInterface;
+use Symfony\Contracts\HttpClient\Exception\TransportExceptionInterface;
 
 class AppFixtures extends Fixture
 {
+    private ApiClient $apiClient;
+
+    public function __construct(ApiClient $apiClient)
+    {
+        $this->apiClient = $apiClient;
+    }
+
+    /**
+     * @throws TransportExceptionInterface
+     * @throws ServerExceptionInterface
+     * @throws RedirectionExceptionInterface
+     * @throws DecodingExceptionInterface
+     * @throws ClientExceptionInterface
+     */
     public function load(ObjectManager $manager): void
     {
-        // $product = new Product();
-        // $manager->persist($product);
+        $countriesList = $this->getCountriesListFromApi();
 
+        foreach ($countriesList as $item) {
+            $country = (new Country())
+                ->setName($item['Country'])
+                ->setSlug($item['Slug'])
+                ->setCode($item['ISO2'])
+                ->setCreatedAt(new DateTimeImmutable());
+            $manager->persist($country);
+        }
         $manager->flush();
+    }
+
+    /**
+     * @throws TransportExceptionInterface
+     * @throws ServerExceptionInterface
+     * @throws RedirectionExceptionInterface
+     * @throws DecodingExceptionInterface
+     * @throws ClientExceptionInterface
+     */
+    private function getCountriesListFromApi(): array
+    {
+        return $this->apiClient->getCountriesList();
     }
 }
