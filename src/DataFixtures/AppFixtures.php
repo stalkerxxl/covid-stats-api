b83f2ce30a22af30796eaaa7fc7d3c7356131eea
Kartulin <stalkerxxl@gmail.com>
2022-11-27 15:51:40 +0400

    refactoring AppFixtures.php to sync-job

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -2,28 +2,19 @@
 
 namespace App\DataFixtures;
 
-use App\Entity\Country;
 use App\Exception\ApiException;
-use App\Service\ApiClient;
-use DateTimeImmutable;
+use App\Message\UpdateCountriesList;
+use App\MessageHandler\UpdateCountriesListHandler;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
-use Symfony\Component\Validator\Validator\ValidatorInterface;
-use Symfony\Contracts\HttpClient\Exception\ClientExceptionInterface;
-use Symfony\Contracts\HttpClient\Exception\DecodingExceptionInterface;
-use Symfony\Contracts\HttpClient\Exception\RedirectionExceptionInterface;
-use Symfony\Contracts\HttpClient\Exception\ServerExceptionInterface;
-use Symfony\Contracts\HttpClient\Exception\TransportExceptionInterface;
 
 class AppFixtures extends Fixture
 {
-    private ApiClient $apiClient;
-    private ValidatorInterface $validator;
+    private UpdateCountriesListHandler $countryHandler;
 
-    public function __construct(ApiClient $apiClient, ValidatorInterface $validator)
+    public function __construct(UpdateCountriesListHandler $countriesListHandler)
     {
-        $this->apiClient = $apiClient;
-        $this->validator = $validator;
+        $this->countryHandler = $countriesListHandler;
     }
 
     /**
@@ -31,29 +22,11 @@ class AppFixtures extends Fixture
      */
     public function load(ObjectManager $manager): void
     {
-        $countriesList = $this->getCountriesListFromApi();
-
-        foreach ($countriesList as $item) {
-            $country = (new Country())
-                ->setName($item['Country'])
-                ->setSlug($item['Slug'])
-                ->setCode($item['ISO2'])
-                ->setCreatedAt(new DateTimeImmutable());
-            $errors = $this->validator->validate($country);
-            if ($errors->count() > 0) {
-                continue;
-            }
-
-            $manager->persist($country);
-        }
-        $manager->flush();
+        $this->makeCountries();
     }
 
-    /**
-     * @throws ApiException
-     */
-    private function getCountriesListFromApi(): array
+    private function makeCountries()
     {
-        return $this->apiClient->getCountriesList();
+        call_user_func($this->countryHandler, new UpdateCountriesList());
     }
 }
